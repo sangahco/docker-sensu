0266141ed465e4bdd0c9909f176167f657edf99e
Emanuele Disco <emawind84@gmail.com>
2018-07-03 17:20:51 +0900

    With RabbitMQ Docker image

--- sensu-server/docker-entrypoint.sh
+++ sensu-server/docker-entrypoint.sh
@@ -6,23 +6,10 @@ set -e
 sed -i s/RABBITMQ_USER/${SENSU_USER}/g /etc/sensu/conf.d/rabbitmq.json
 sed -i s/RABBITMQ_PWD/${SENSU_PASSWORD}/g /etc/sensu/conf.d/rabbitmq.json
 
-sed -i s/RABBITMQ_USER/${SENSU_USER}/g /etc/rabbitmq/rabbitmq.config
-sed -i s/RABBITMQ_PWD/${SENSU_PASSWORD}/g /etc/rabbitmq/rabbitmq.config
-
 /etc/init.d/redis-server start
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start redis: $status"
-  exit $status
-fi
-
-/etc/init.d/rabbitmq-server start
-# rabbitmqctl add_vhost /sensu
-# rabbitmqctl add_user sensu secret
-# rabbitmqctl set_permissions -p /sensu sensu ".*" ".*" ".*"
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start rabbitmq: $status"
   exit $status
 fi
 
