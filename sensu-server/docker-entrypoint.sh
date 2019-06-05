04db93a85d9908d1191e726032165c67fa4eef1b
Emanuele Disco <emawind84@gmail.com>
2019-06-05 09:32:17 +0900

    fix for slack webhook

--- sensu-server/docker-entrypoint.sh
+++ sensu-server/docker-entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 # replace rabbitMQ credentials
 sed -i s/RABBITMQ_USER/${SENSU_USER}/g /etc/sensu/conf.d/rabbitmq.json
 sed -i s/RABBITMQ_PWD/${SENSU_PASSWORD}/g /etc/sensu/conf.d/rabbitmq.json
+sed -i s/SLACK_HANDLER_WEBHOOK/${SLACK_HANDLER_WEBHOOK}/g /etc/sensu/conf.d/slack_handler.json
 
 /etc/init.d/redis-server start
 status=$?
